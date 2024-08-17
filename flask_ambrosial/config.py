0cb72961ba99555929bf7000264f149f2ad9ce35
paschalugwu <ugwupaschal@gmail.com>
2024-08-17 10:31:47 +0100

    style(config): adhere to PEP 8 standards and improve readability
    Refactored the config.py file to comply with PEP 8 standards:
    - Ensured no line exceeds 75 characters.
    - Added efficient comments and standard docstrings.
    - Maintained original functionality.

--- flask_ambrosial/config.py
+++ flask_ambrosial/config.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
+"""
+Configuration module for Flask application settings.
+"""
+
 import os
 
 class Config:
+    """
+    Base configuration class with default settings.
+    """
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -15,6 +22,9 @@ class Config:
     BABEL_TRANSLATION_DIRECTORIES = './translations'
 
 class TestingConfig(Config):
+    """
+    Configuration class for testing environment.
+    """
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
