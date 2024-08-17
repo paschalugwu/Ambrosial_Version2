8f4f6bc02072bebb33ae0f504a6cb2329483df4a
paschalugwu <ugwupaschal@gmail.com>
2024-08-17 11:04:47 +0100

    style(forms): adhere to PEP 8 standards and improve readability
    Refactored the forms.py file to comply with PEP 8 standards:
    - Ensured no line exceeds 75 characters.
    - Added efficient comments and standard docstrings.
    - Maintained original functionality.

--- flask_ambrosial/posts/forms.py
+++ flask_ambrosial/posts/forms.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python3
 
+"""
+Forms for creating posts, comments, and replies in the Flask application.
+"""
+
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
-    """Form for creating a new post."""
+    """
+    Form for creating a new post.
+    """
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
-    image_filename = FileField('Image', validators=[DataRequired(), FileAllowed(['jpg', 'png', 'jpeg'])])
+    image_filename = FileField(
+        'Image', validators=[
+            DataRequired(), FileAllowed(['jpg', 'png', 'jpeg'])
+        ]
+    )
     submit = SubmitField('Post')
 
 class CommentForm(FlaskForm):
-    """Form for creating a new comment."""
+    """
+    Form for creating a new comment.
+    """
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Comment')
 
 class ReplyForm(FlaskForm):
-    """Form for creating a new reply."""
+    """
+    Form for creating a new reply.
+    """
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Reply')
