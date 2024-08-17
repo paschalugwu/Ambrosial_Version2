a416f087feeb0e76103e56580f7ee1d810cd15e0
paschalugwu <ugwupaschal@gmail.com>
2024-08-17 10:49:59 +0100

    style(error_handlers): adhere to PEP 8 standards and improve readability
    Refactored the handlers.py file to comply with PEP 8 standards:
    - Ensured no line exceeds 75 characters.
    - Added efficient comments and standard docstrings.
    - Maintained original functionality.

--- flask_ambrosial/errors/handlers.py
+++ flask_ambrosial/errors/handlers.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Error handlers module for Flask application.
+"""
+
 from flask import Blueprint, render_template
 
 # Create a Blueprint for handling errors
@@ -8,38 +12,44 @@ errors = Blueprint('errors', __name__)
 
 @errors.app_errorhandler(404)
 def error_404(error):
-    """Render a custom 404 error page.
+    """
+    Render a custom 404 error page.
 
     Args:
         error: The error object.
 
     Returns:
-        tuple: A tuple containing the rendered template and the HTTP status code.
+        tuple: A tuple containing the rendered template and the HTTP 
+        status code.
     """
     return render_template('errors/404.html'), 404
 
 
 @errors.app_errorhandler(403)
 def error_403(error):
-    """Render a custom 403 error page.
+    """
+    Render a custom 403 error page.
 
     Args:
         error: The error object.
 
     Returns:
-        tuple: A tuple containing the rendered template and the HTTP status code.
+        tuple: A tuple containing the rendered template and the HTTP 
+        status code.
     """
     return render_template('errors/403.html'), 403
 
 
 @errors.app_errorhandler(500)
 def error_500(error):
-    """Render a custom 500 error page.
+    """
+    Render a custom 500 error page.
 
     Args:
         error: The error object.
 
     Returns:
-        tuple: A tuple containing the rendered template and the HTTP status code.
+        tuple: A tuple containing the rendered template and the HTTP 
+        status code.
     """
     return render_template('errors/500.html'), 500
