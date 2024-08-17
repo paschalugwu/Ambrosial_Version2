80942fc0c2afd19ad6fce102579b68210d4ecdd4
paschalugwu <ugwupaschal@gmail.com>
2024-08-17 10:56:54 +0100

    style(routes): adhere to PEP 8 standards and improve readability
    Refactored the routes.py file to comply with PEP 8 standards:
    - Ensured no line exceeds 75 characters.
    - Added efficient comments and standard docstrings.
    - Maintained original functionality.

--- flask_ambrosial/main/routes.py
+++ flask_ambrosial/main/routes.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Main routes module for the Flask application.
+"""
+
 from flask import Blueprint, render_template, url_for, request
 from flask_ambrosial.models import Post
 
@@ -9,7 +13,8 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    """Render the home page.
+    """
+    Render the home page.
 
     Returns:
         str: Rendered template with posts and associated image files.
@@ -17,16 +22,23 @@ def home():
     # Get the page number from the request arguments, default is 1
     page = request.args.get('page', 1, type=int)
     # Query posts ordered by date posted in descending order, paginated
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
+    posts = Post.query.order_by(
+        Post.date_posted.desc()
+    ).paginate(page=page, per_page=3)
     # Generate URLs for associated image files
-    image_files = [url_for('static', filename='post_pics/' + post.image_filename) for post in posts.items]
+    image_files = [
+        url_for('static', filename='post_pics/' + post.image_filename)
+        for post in posts.items
+    ]
     # Render the home template with posts and image files
-    return render_template('home.html', posts=posts, image_files=image_files)
-
+    return render_template(
+        'home.html', posts=posts, image_files=image_files
+    )
 
 @main.route("/about")
 def about():
-    """Render the about page.
+    """
+    Render the about page.
 
     Returns:
         str: Rendered template for the about page.
