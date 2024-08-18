1ed4c3014b9d47279ab4be1dd9266de319a5b23c
Paschal Ugwu <106428248+paschalugwu@users.noreply.github.com>
2024-08-18 14:29:09 +0000

    Update on requirement file

--- run.py
+++ run.py
@@ -6,8 +6,11 @@ from flask_ambrosial import create_app, socketio
 app = create_app(use_socketio=True)
 
 if __name__ == '__main__':
-    # Run the Flask application with SocketIO in production mode
+    # Run the Flask application with SocketIO
+    # - debug=True enables debug mode for development
+    # - This means the server will reload on code changes and
+    #   provide more detailed error messages
     if socketio:
-        socketio.run(app, host="0.0.0.0", port=5000)
+        socketio.run(app, debug=True)
     else:
-        app.run(host="0.0.0.0", port=5000)
+        app.run(debug=True)
