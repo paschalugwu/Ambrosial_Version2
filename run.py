a55ffa959bbb133b43e51cbf81a44f02f3a1958b
paschalugwu <ugwupaschal@gmail.com>
2024-08-19 11:10:37 +0100

    Troubleshoot websocket

--- run.py
+++ run.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 """Entry point for running the Flask application."""
 
-from flask_ambrosial import create_app, socketio
+from flask_ambrosial import create_app
 
-app = create_app(use_socketio=True)
+app = create_app()
 
+# Vercel expects the app to be a module-level variable named `app`
 if __name__ == '__main__':
-    # Run the Flask application with SocketIO
-    # - debug=True enables debug mode for development
-    # - This means the server will reload on code changes and
-    #   provide more detailed error messages
-    if socketio:
-        socketio.run(app, debug=True)
-    else:
-        app.run(debug=True)
+    app.run(debug=True)
